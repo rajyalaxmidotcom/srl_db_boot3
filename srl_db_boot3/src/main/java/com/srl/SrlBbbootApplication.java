694aa5a61849c733400befdcc604633c541acff6
Abhi <Abhi@DESKTOP-M3Q06NL>
2024-07-08 12:33:39 +0530

    first commit

---
+++ srl_db_boot3/src/main/java/com/srl/SrlBbbootApplication.java
@@ -0,0 +1,13 @@
+package com.srl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SrlBbbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SrlBbbootApplication.class, args);
+	}
+
+}
